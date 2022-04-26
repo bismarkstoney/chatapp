a175e72afd2d869fa60e3ffea0dd8fae417d2815
bismarkstoney <bismarkstoney@yahoo.com>
2022-04-26 13:07:31 +0000

    changes to folder

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const path =require('path')
+const path = require('path');
 const cors = require('cors');
 const passport = require('passport');
 const expressSession = require('express-session');
@@ -26,11 +26,11 @@ if (app.get('env') === 'production') {
 if (process.env.NODE_ENV === 'production') {
 	// Set static folder
 	app.use(express.static('frontend/build'));
-  
+
 	app.get('*', (req, res) => {
-	  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
 	});
-  }
+}
 app.use(expressSession(session));
 passport.use(strategy);
 app.use(passport.initialize());
@@ -57,12 +57,7 @@ app.use('/api/v1/messages', messageRoutes);
 const server = app.listen(process.env.PORT, () =>
 	console.log(`Server started on ${PORT}`)
 );
-const io = socket(server, {
-	cors: {
-		origin: 'http://localhost:3000',
-		credentials: true,
-	},
-});
+const io = socket(server);
 
 global.onlineUsers = new Map();
 io.on('connection', (socket) => {

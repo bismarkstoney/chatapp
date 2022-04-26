e131a1e4a47f7dfd955d242ba3f45c9f1c4b4bd1
bismarkstoney <bismarkstoney@yahoo.com>
2022-04-26 13:27:02 +0000

    import changes

--- index.js
+++ index.js
@@ -2,24 +2,24 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const passport = require('passport');
-const expressSession = require('express-session');
+//const passport = require('passport');
+//const expressSession = require('express-session');
 const socket = require('socket.io');
 const authRoutes = require('./routes/auth');
 const messageRoutes = require('./routes/messages');
-const strategy = require('./middleware/passport');
+//const strategy = require('./middleware/passport');
 const connectDb = require('./config/db');
 
 //app initialization
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const session = {
-	secret: process.env.secret,
-	cookie: {},
-	resave: false,
-	saveUninitialized: false,
-};
+// const session = {
+// 	secret: process.env.secret,
+// 	cookie: {},
+// 	resave: false,
+// 	saveUninitialized: false,
+// };
 if (app.get('env') === 'production') {
 	session.cookie.secure = true; // Serve secure cookies, requires HTTPS
 }
@@ -31,18 +31,11 @@ if (process.env.NODE_ENV === 'production') {
 		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
 	});
 }
-app.use(expressSession(session));
-passport.use(strategy);
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(expressSession(session));
+// passport.use(strategy);
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-passport.serializeUser((user, done) => {
-	done(null, user);
-});
-
-passport.deserializeUser((user, done) => {
-	done(null, user);
-});
 //middlewares
 app.use(cors());
 app.use(express.json());
